f8e4b77e9598e8510482bcc57cfd5ac268218352
oscar <gallardo_oscar@outlook.com>
2021-11-09 12:30:59 -0600

    adding comments to both js and css files

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
+// get the elements needed
 let hamburger = document.querySelector('.hamburger');
 let menu = document.querySelector('.navigation__menu');
 
-hamburger.addEventListener('click', (event) => {
+// event binding for the mobile hamburger icon
+hamburger.addEventListener('click', toggleMenu);
+
+// function to toggle menu on / off
+function toggleMenu() {
+    // if the hamburger icon doesn't contain is-active class
     if(hamburger.classList.contains('is-active') === false) {
+        // add is-active class to hamburger icon to trigger animation to the close icon
         hamburger.classList.add('is-active');
+        // add is-active class to open the menu
         menu.classList.add('is-active');
     } else {
+        // remove is-active class to hamburger icon to trigger animation to revert to the hamburger icon
         hamburger.classList.remove('is-active');
+        // remove is-active class to close the menu
         menu.classList.remove('is-active');
     }
-});
+}
