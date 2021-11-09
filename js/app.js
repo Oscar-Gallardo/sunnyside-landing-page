0a2fa5aff1de674c5b7c5273ba37aa17ba32aa6d
oscar <gallardo_oscar@outlook.com>
2021-11-09 11:37:09 -0600

    added script to toggle the nav menu class state

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.navigation__menu');
+
 hamburger.addEventListener('click', (event) => {
     if(hamburger.classList.contains('is-active') === false) {
         hamburger.classList.add('is-active');
+        menu.classList.add('is-active');
     } else {
         hamburger.classList.remove('is-active');
+        menu.classList.remove('is-active');
     }
 });
